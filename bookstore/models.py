94e5222a5a7ed48db3a899a0d61ed99c294284d2
Johan Travieso Castro <jtravieso8863@gmail.com>
2026-07-20 15:46:24 -0400

    book model

--- bookstore/models.py
+++ bookstore/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(unique=True, max_length=50)
+    pub_date = models.DateTimeField()
+    editorial_name = models.CharField(unique=True, max_length=100)
+    editorial_address = models.TextField()
+
+    def __str__(self):
+        return self.title
+
