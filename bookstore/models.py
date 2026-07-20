a228eee7f6c7af02362f8518afb847075fcf0d77
Johan Travieso Castro <jtravieso8863@gmail.com>
2026-07-20 17:15:44 -0400

    automatic migration to remove old editorial attr on book

--- bookstore/models.py
+++ bookstore/models.py
@@ -10,11 +10,9 @@ class Editorial(models.Model):
 
 
 class Book(models.Model):
-    editorial = models.ForeignKey(Editorial, on_delete=models.CASCADE, null=True)
+    editorial = models.ForeignKey(Editorial, on_delete=models.CASCADE)
     title = models.CharField(unique=True, max_length=50)
     pub_date = models.DateTimeField()
-    editorial_name = models.CharField(max_length=100)
-    editorial_address = models.TextField()
 
     def __str__(self):
         return self.title
