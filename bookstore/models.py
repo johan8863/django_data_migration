9e5883464c13f5a34185d7b3c7a6b7c517a422e5
Johan Travieso Castro <jtravieso8863@gmail.com>
2026-07-20 16:06:26 -0400

    editorial object nullable foreign key approach

--- bookstore/models.py
+++ bookstore/models.py
@@ -1,7 +1,16 @@
 from django.db import models
 
 
+class Editorial(models.Model):
+    name = models.CharField(unique=True, max_length=50)
+    address = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
 class Book(models.Model):
+    editorial = models.ForeignKey(Editorial, on_delete=models.CASCADE, null=True)
     title = models.CharField(unique=True, max_length=50)
     pub_date = models.DateTimeField()
     editorial_name = models.CharField(max_length=100)
