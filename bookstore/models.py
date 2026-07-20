5cb51bc6437ddafc979e874f49b62329c6d2b2fd
Johan Travieso Castro <jtravieso8863@gmail.com>
2026-07-20 16:00:39 -0400

    rolled back uniqunes until new model

--- bookstore/models.py
+++ bookstore/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Book(models.Model):
     title = models.CharField(unique=True, max_length=50)
     pub_date = models.DateTimeField()
-    editorial_name = models.CharField(unique=True, max_length=100)
+    editorial_name = models.CharField(max_length=100)
     editorial_address = models.TextField()
 
     def __str__(self):
